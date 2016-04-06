62140a612d24f257c51354e60acc902ea42a9995
rajgaikwad001 <rajkumar.gaikwad@crest-premedia.in>
2016-04-06 17:31:19 +0530

    project checkin

---
+++ SampleWin/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleWin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var a = textBox1.Text;
+            var b = textBox2.Text;
+            var c = a + b;
+            MessageBox.Show(c);
+        }
+    }
+}
